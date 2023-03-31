e2b1156441c6c316a7eab3093afd85914b1c830d
Markus Kurbel <m4rku@pm.me>
2023-03-31 22:34:13 +0200

    Further login page styling

--- components/Auth.vue
+++ components/Auth.vue
@@ -20,22 +20,34 @@ const handleLogin = async () => {
 
 <template>
   <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">Supabase + Nuxt 3</h1>
+    <div class="col-6 form-widget login">
+      <img src="../assets/logo.png" class="logo"/>
+      <h1 class="header">Performance Health</h1>
       <p class="description">Sign in via magic link with your email below</p>
       <div>
         <v-text-field variant="outlined" class="inputField" type="email" placeholder="Your email" v-model="email" />
       </div>
       <div>
-        <v-btn
-          variant="outlined"
+        <input
           type="submit"
           class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
           :disabled="loading"
-        >
-        {{ loading ? 'Loading' : 'Send magic link' }}
-        </v-btn>
+        />
       </div>
     </div>
   </form>
 </template>
+
+<style scoped>
+.logo {
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  height: 50%;
+}
+
+.login {
+  text-align: center;
+}
+</style>
