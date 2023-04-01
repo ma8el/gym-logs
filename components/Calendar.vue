129dcf1c850fe23c5fe3217b2e87c23703329224
Markus Kurbel <m4rku@pm.me>
2023-04-01 14:21:40 +0200

    Add basic calendar

---
+++ components/Calendar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import FullCalendar from '@fullcalendar/vue3'
+  import interactionPlugin from '@fullcalendar/interaction'
+  import timeGridPlugin from '@fullcalendar/timegrid'
+  import dayGridPlugin from '@fullcalendar/daygrid'
+
+  const headerToolbar = {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  }
+
+  const calendarOptions = {
+    plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
+    initialView: 'timeGridWeek',
+    headerToolbar: headerToolbar,
+    themeSystem: 'bootstrap5',
+    nowIndicator: true,
+    editable: true,
+    selectable: true,
+    locale: 'en',
+    firstDay: 1,
+    initialEvents: [
+      { title: 'nice event', start: new Date() }
+    ]
+  }
+</script>
+<template>
+    <FullCalendar :options='calendarOptions'/>
+</template>
+
+<style scoped>
+.fc {
+  max-width: 900px;
+}
+
+</style>
