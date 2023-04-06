a741c9e6dde80ca17b5dfefa234187a91db1dd13
Markus Kurbel <m4rku@pm.me>
2023-04-07 00:26:43 +0200

    Change expansion panel to tabs

--- components/WorkoutsExpansionPanel.vue
+++ components/WorkoutsExpansionPanel.vue
@@ -46,42 +46,63 @@
   onUnmounted(() => {
     channel.unsubscribe()
   })
+  const tab = ref('')
 </script>
 
 <template>
-  <div v-if="workouts">
-    <v-expansion-panels 
-      variant="accordion"
-      class="workout-panels"
-      v-for="workout in workouts"
-      :key="workout.id"
+  <div v-if="workouts" class="d-flex flex-row">
+    <v-tabs
+      v-model="tab"
+      direction="vertical"
+      color="orange"
     >
-      <v-expansion-panel>
-        <v-expansion-panel-title>{{ workout.name }}</v-expansion-panel-title>
-        <v-expansion-panel-text>
-          <v-row>
-            <v-col>
-              <b>Description:</b> {{ workout.description }}
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col class="justify-end">
-              <b>Created at:</b> {{ new Date(workout.created_at).toLocaleString('en-US', {timeZone: 'UTC'}) }}
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="3">
-              <v-btn @click="addExercise()">
-                add exercise
-              </v-btn>
-            </v-col>
-          </v-row>
-          <div v-for="exerciseID in exerciseIDs" :key="exerciseID">
-            <WorkoutExerciseForm :id="exerciseID" @close-form="closeExerciseForm"/>
-            <v-spacer></v-spacer>
-          </div>
-
-        </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
+      <v-tab 
+        v-for="workout in workouts"
+        :key="workout.id"
+        :value="workout.name">
+        {{  workout.name }}
+      </v-tab>
+    </v-tabs>
+    <div class="full-width-card">
+    <v-card>
+      <v-window
+        v-for="workout in workouts"
+        :key="workout.id"
+        v-model="tab">
+        <v-window-item :value="workout.name">
+          <v-card flat>
+            <v-card-text>
+              <v-row>
+                <v-col cols="10">
+                  <b>Description:</b> {{ workout.description }}
+                </v-col>
+                <v-spacer></v-spacer>
+                <v-col cols="2">
+                  <b>Created at:</b> {{ new Date(workout.created_at).toLocaleString('en-US', {timeZone: 'UTC'}) }}
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="3">
+                  <v-btn @click="addExercise()">
+                    add exercise
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <div v-for="exerciseID in exerciseIDs" :key="exerciseID">
+                <WorkoutExerciseForm :id="exerciseID" @close-form="closeExerciseForm"/>
+                <v-spacer></v-spacer>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-window-item>
+      </v-window>
+    </v-card>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.full-width-card {
+  width: 90%;
+}
+</style>
