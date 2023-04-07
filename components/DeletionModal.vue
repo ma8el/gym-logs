9cba13727cb1176398e9f2db221580572a7402b7
Markus Kurbel <m4rku@pm.me>
2023-04-07 08:44:14 +0200

    Add workout deletion

---
+++ components/DeletionModal.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  const emit = defineEmits(['close-workout-form', 'delete-workout'])
+
+  const emitClose = () => {
+    emit('close-workout-form')
+  }
+
+  const emitDelete = () => {
+    emit('delete-workout')
+  }
+</script>
+
+<template>
+  <v-card>
+    <v-card-title>
+      Are you sure you want to delete this item?
+    </v-card-title>
+    <v-card-text>
+      This action cannot be undone.
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        variant="text"
+        @click="emitDelete"
+      >
+        Delete
+      </v-btn>
+      <v-btn
+        color="primary"
+        variant="text"
+        @click="emitClose"
+      >
+        Close
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- components/WorkoutsTabs.vue
+++ components/WorkoutsTabs.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
   const supabase = useSupabaseClient()
+  const workoutsTable = 'workouts'
   
   const exerciseIDs = ref<number[]>([])
   const exerciseID = ref(0)
   const workouts = ref()
+
+  const deletionDialog = ref(false)
+
+  const tab = ref('')
   
   const closeExerciseForm = (id: number) => {
     exerciseIDs.value = exerciseIDs.value.filter(exerciseID => exerciseID !== id)
@@ -13,10 +18,10 @@
     exerciseIDs.value.push(exerciseID.value++)
   }
 
-  const loadWorkouts = () => {
-    supabase
-      .from('workouts')
-      .select(`id, name, description, created_at`)
+  const loadWorkouts = async () => {
+    await supabase
+      .from(workoutsTable)
+      .select(`id, name, description, created_at, updated_at`)
       .then(({ data, error }) => {
         if (error) {
           console.log('error', error)
@@ -26,6 +31,19 @@
       })
   }
 
+  const deleteWorkout = async (workoutId: number) => {
+    const { error } = await supabase
+      .from(workoutsTable)
+      .delete()
+      .eq('id', workoutId)
+    if (error) {
+      console.log('error', error)
+    } else {
+      deletionDialog.value = false
+      loadWorkouts()
+    }
+  }
+
   const channel = supabase
     .channel('table-db-changes')
     .on(
@@ -33,7 +51,7 @@
       {
         event: '*',
         schema: 'public',
-        table: 'workouts',
+        table: workoutsTable,
       },
       () => loadWorkouts()
     )
@@ -46,7 +64,7 @@
   onUnmounted(() => {
     channel.unsubscribe()
   })
-  const tab = ref('')
+
 </script>
 
 <template>
@@ -71,19 +89,43 @@
         v-model="tab">
         <v-window-item :value="workout.name">
           <v-card flat>
+            <v-toolbar density="compact" color="orange">
+              <v-toolbar-title></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn
+                icon="$delete"
+                size="small"
+                variant="text"
+                @click="deletionDialog = true"
+              ></v-btn>
+              <v-dialog
+                v-model="deletionDialog"
+                width="auto"
+              >
+              <DeletionModal 
+                @delete-workout="deleteWorkout(workout.id)"
+                @close-workout-form="deletionDialog = false"
+              />
+              </v-dialog>
+            </v-toolbar>
             <v-card-text>
               <v-row>
-                <v-col cols="10">
+                <v-col cols="8">
                   <b>Description:</b> {{ workout.description }}
                 </v-col>
                 <v-spacer></v-spacer>
-                <v-col cols="2">
+                <v-col cols="4">
                   <b>Created at:</b> {{ new Date(workout.created_at).toLocaleString('en-US', {timeZone: 'UTC'}) }}
                 </v-col>
+              <v-row justify="end">
+                <v-col cols="4">
+                  <b>Last modified:</b> {{ new Date(workout.updated_at).toLocaleString('en-US', {timeZone: 'UTC'}) }}
+                </v-col>
+              </v-row>
               </v-row>
               <v-row>
                 <v-col cols="3">
-                  <v-btn @click="addExercise()">
+                  <v-btn color="orange" @click="addExercise()">
                     add exercise
                   </v-btn>
                 </v-col>
