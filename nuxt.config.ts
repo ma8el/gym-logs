bb08f9eecfa7a1b10ccffa320afab3670cf413a8
Markus Kurbel <m4rku@pm.me>
2023-04-09 18:40:41 +0200

    Add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/supabase'],
+  modules: ['@nuxtjs/supabase','@pinia/nuxt'],
+  pinia: {
+    autoImports: [
+      'defineStore',
+      ['defineStore', 'definePiniaStore'],
+    ],
+  },
   css: ['~/assets/main.css', 
         'vuetify/lib/styles/main.sass',
         '@mdi/font/css/materialdesignicons.css',
@@ -8,4 +14,8 @@ export default defineNuxtConfig({
   build: {
     transpile: ['vuetify', 'chart.js', '@vuepic/vue-datepicker'],
   },
+  imports: {
+    // Auto-import pinia stores defined in `~/stores`
+    dirs: ['stores']
+  }
 })
