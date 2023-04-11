42984b54ac9a658da57ae316963cbb6851c19b1c
Markus Kurbel <m4rku@pm.me>
2023-04-11 23:09:30 +0200

    Draft of you page

--- pages/you.vue
+++ pages/you.vue
@@ -27,44 +27,27 @@
 
 <template>
   <NuxtLayout name="app">
-    <h1>You</h1>
-    <div class="form">
+    <h1>Goals</h1>
       <form  @submit.prevent="updateProfile">
-        <h2>Body Metrics</h2>
-        <v-row no-gutters>
-          <v-text-field variant="outlined" label="Weight (in kg)" v-model="userStatistics.weight"/>
-          <v-text-field variant="outlined" label="Height (in cm)" v-model="userStatistics.height" />
-          <v-text-field variant="outlined" label="Age" v-model="userStatistics.age" />
-          <v-select variant="outlined" :items="genderItems" label="Gender" v-model="userStatistics.gender" />
+        <v-row>
+          <h2>Weight</h2>
         </v-row>
-        <h2>Gym Performance</h2>
-        <v-row no-gutters>
+        <v-row>
+          <v-text-field variant="outlined" label="Weight (in kg)" v-model="userStatistics.weight"/>
+        </v-row>
+        <v-row>
+          <h2>Gym Performance</h2>
+        </v-row>
+        <v-row>
           <v-text-field variant="outlined" label="Squat 1RM (in kg)" v-model="userStatistics.performance.squat"/>
           <v-text-field variant="outlined" label="Deadlift 1RM (in kg)" v-model="userStatistics.performance.deadlift"/>
           <v-text-field variant="outlined" label="Bench 1RM (in kg)" v-model="userStatistics.performance.bench"/>
           <v-text-field variant="outlined" label="Overhead 1RM (in kg)" v-model="userStatistics.performance.overhead"/>
         </v-row>
-        <div>
-          <input
-            type="submit"
-            class="button primary block"
-            :value="loading ? 'Loading ...' : 'Save'"
-            :disabled="loading"
-          />
-        </div>
       </form>
-    </div>
-
   </NuxtLayout>
 </template>
 
 <style scoped>
-.form {
-  margin: 10px 10px 10px 0;
-}
-
-form h2 {
-  margin-bottom: 10px;
-}
 
 </style>
