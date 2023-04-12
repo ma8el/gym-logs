7a1ff9bfd3e44cac28918ec008d86a93319263b7
Markus Kurbel <m4rku@pm.me>
2023-04-12 22:59:07 +0200

    Add weight store

--- stores/user.ts
+++ stores/user.ts
@@ -10,3 +10,34 @@ export const useUserStore = defineStore('user', {
       }
     }
   })
+
+export const useWeightStore = defineStore('weight', {
+  state: () => ({
+    weights: []
+  }),
+
+  actions: {
+    async fetchWeights() {
+      const supabase = useSupabaseClient()
+      const { data, error } = await supabase
+        .from('weights')
+        .select('weight, created_at')
+        .order('created_at', { ascending: false })
+      if (error) throw error
+      this.weights = data
+    }
+  },
+  getters : {
+    getLatestWeight: (state) => async () => {
+      if (state.weights.length === 0) {
+        return null
+      }
+      const data = await state.weights.reduce((prev, current) => {
+        const prevDate = new Date(prev.created_at);
+        const currentDate = new Date(current.created_at);
+        return (prevDate > currentDate) ? prev : current
+      })
+      return data
+    }
+  }
+})
