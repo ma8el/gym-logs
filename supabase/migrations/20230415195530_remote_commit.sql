48b46483fe42034dc7644dd8f0c50a8312786f8f
Markus Kurbel <m4rku@pm.me>
2023-04-15 22:07:28 +0200

    Add weight sequence

--- supabase/migrations/20230415195530_remote_commit.sql
+++ supabase/migrations/20230415195530_remote_commit.sql
@@ -1,26 +1,3 @@
-create type "auth"."code_challenge_method" as enum ('s256', 'plain');
-
-create table "auth"."flow_state" (
-    "id" uuid not null,
-    "user_id" uuid,
-    "auth_code" text not null,
-    "code_challenge_method" auth.code_challenge_method not null,
-    "code_challenge" text not null,
-    "provider_type" text not null,
-    "provider_access_token" text,
-    "provider_refresh_token" text,
-    "created_at" timestamp with time zone,
-    "updated_at" timestamp with time zone
-);
-
-
-CREATE UNIQUE INDEX flow_state_pkey ON auth.flow_state USING btree (id);
-
-CREATE INDEX idx_auth_code ON auth.flow_state USING btree (auth_code);
-
-alter table "auth"."flow_state" add constraint "flow_state_pkey" PRIMARY KEY using index "flow_state_pkey";
-
-
 drop policy "Users can insert weigths." on "public"."weight";
 
 drop policy "Users can update own weight." on "public"."weight";
@@ -30,6 +7,8 @@ drop policy "Weigths are viewable by user." on "public"."weight";
 alter table "public"."weight" drop constraint "weight_user_id_fkey";
 
 drop table "public"."weight";
+
+create sequence "weight_id_seq" start 1;
 
 create table "public"."weights" (
     "id" integer not null default nextval('weight_id_seq'::regclass),
